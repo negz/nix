f6803f1ea458051f57214f9c2173b3cbfd9e21a4
Nic Cope <nicc@rk0n.org>
2022-03-23 11:13:48 -0700

    Enable docker
    Sadly I'm giving up on containerd - forgot that all the crossplane build
    tooling assumes docker. :(
    
    Signed-off-by: Nic Cope <nicc@rk0n.org>

--- configuration.nix
+++ configuration.nix
@@ -46,7 +46,7 @@
     defaultUserShell = pkgs.zsh;
     users.negz = {
       isNormalUser = true;
-      extraGroups = [ "wheel" ];
+      extraGroups = [ "wheel", "docker" ];
       openssh.authorizedKeys.keys = [
         "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDEnexRk7YdGNbOOKgZQSzL1/x84/1NNl8m/oWWCjCk2xYZAr8iBFPtFFEWPrdJ+OEJjGrInhqJBkICoF0R4rsHojDMwukbuJT9sFXBzpNwkaP+DPgFzao7FYNoAb555f+JmGEXqvSJTs3crmYQdS09Yy4HqMgCUuYIA987kWQ8LT068pphQTCAX0WyRg7pNF2GSuW6EnGJYJBo081/AVoGuWuF+ciIAN0/q1YcPbQoTPa+Hiu/jLo7rMXeU+PjX4v+fH22kcuQDR6APpOqmB7b9opTOBepy1tDogJwYQNCpFW/gQMsbWcj9kxRe/hNoyCi20iFnJOXQzP393kfEGT5tllYHDpRCaVUUMREtF630A+IAZASRDiAZq/oLZK1mLhwM9KOu4BYBmt0glxLZXt6dsgUD4y7KrLLghrXBXi+aNP4sKKeFzIGH5P1ZqL9dAAAVdjr+yRYIWw0XGVG9FE4qlOfzXtc9e/v2IIubzjx/Cu5LgzakPTaDgDa/nUaIQDCDS95bNa8t/nh1/rLqI/qb3mSKFlY0Z5aDyTjIxDmwuwQQa04zmDKUjgDaZKwoo8gQMVVU5g2fyrC+xK21exffAcPtguIr0x7Z5aeY1dekNESHsaMwzOWLJowIHIWKRLyMlUiuoXmh/FGwWTauSmFF9XJ9GikzT9X9Sz1cIxwMQ== negz@rk0n.org"
       ];
@@ -65,6 +65,10 @@
   programs = {
     zsh.enable = true;
     vim.defaultEditor = true;
+  };
+
+  virtualisation = {
+    docker.enable = true;
   };
 
   environment = {
