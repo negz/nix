4a701855d4705bdaea7eeb57c7b772de6c995fe5
Nic Cope <nicc@rk0n.org>
2022-03-23 02:47:48 -0700

    Give up on containers in containers :)
    Signed-off-by: Nic Cope <nicc@rk0n.org>

--- configuration.nix
+++ configuration.nix
@@ -70,37 +70,4 @@
     nixos.enable = true;
     man.enable = true;
   };
-
-  containers = {
-    containerd = {
-      autoStart = true;
-      config = { config, pkgs, ... }: {
-        virtualisation.containerd.enable = true;
-        environment = {
-          defaultPackages = lib.mkForce [];
-          systemPackages = [ pkgs.nerdctl ];
-        };
-        documentation = {
-          enable = false;
-          nixos.enable = false;
-          man.enable = false;
-        };
-      };
-    };
-    k3s = {
-      autoStart = true;
-      config = { config, pkgs, ... }: {
-        services.k3s.enable = true;
-        environment = {
-          defaultPackages = lib.mkForce [];
-          systemPackages = [ pkgs.nerdctl ];
-        };
-        documentation = {
-          enable = false;
-          nixos.enable = false;
-          man.enable = false;
-        };
-      };
-    };
-  };
 }
