f293c479ed8794be7ae55f46966ca0743aa091e9
Nic Cope <nicc@rk0n.org>
2022-03-23 00:47:44 -0700

    Do some flakeception
    Signed-off-by: Nic Cope <nicc@rk0n.org>

--- configuration.nix
+++ configuration.nix
@@ -10,6 +10,9 @@
 
   nix = {
     package = pkgs.nixUnstable;
+    extraOptions = ''
+      experimental-features = nix-command flakes
+    '';
     settings.auto-optimise-store = true;
     gc.automatic = true;
   };
