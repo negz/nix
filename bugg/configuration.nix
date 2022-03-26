05695ad35a9f0dbaccb2c0aac3edb49777fd7d6b
Nic Cope <nicc@rk0n.org>
2022-03-25 19:02:02 -0700

    Use 'stable' nix
    We're already using the unstable nixpkgs, so I presume this is about the
    same as nixUnstable in 21.11?
    
    Signed-off-by: Nic Cope <nicc@rk0n.org>

--- bugg/configuration.nix
+++ bugg/configuration.nix
@@ -4,7 +4,19 @@
   environment.systemPackages = [ pkgs.vim ];
 
   services.nix-daemon.enable = true;
-  nix.package = pkgs.nixUnstable;
+
+  nix = {
+    package = pkgs.nix;
+    extraOptions = ''
+      experimental-features = nix-command flakes
+    '';
+    gc.automatic = true;
+  };
+
+  nixpkgs = {
+    config.allowUnfree = true;
+    config.allowUnsupportedSystem = true;
+  };
 
   programs.zsh.enable = true; 
 
