317b9b4b0eb8c84540184fddef18d3be8d4ab233
Nic Cope <nicc@rk0n.org>
2022-03-26 01:32:18 -0700

    Setup zsh, maybe?
    Signed-off-by: Nic Cope <nicc@rk0n.org>

--- users/negz/configuration.nix
+++ users/negz/configuration.nix
@@ -1,6 +1,26 @@
 { config, lib, pkgs, ... }:
+
 {
-  home.packages = [
-    pkgs.jq
-  ];
+  # TODO(negz): Just use nix-env manually?
+  home.packages = [];
+
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+    enableAutosuggestions = true;
+    enableSyntaxHighlighting = true;
+    defaultKeymap = "emacs";
+    plugins = [
+      {
+        name = "powerlevel10k";
+        src = pkgs.zsh-powerlevel10k;
+        file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
+      }
+      {
+        name = "powerlevel10k-config";
+        src = lib.cleanSource ./p10k.zsh;
+        file = ".p10k.zsh";
+     }
+    ];
+  };
 }
