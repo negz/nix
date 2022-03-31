f0296658b43672c36d5fb53dba985e99c1220cae
Nic Cope <nicc@rk0n.org>
2022-03-30 21:55:20 -0700

    Group programs
    Signed-off-by: Nic Cope <nicc@rk0n.org>

--- users/negz/configuration.nix
+++ users/negz/configuration.nix
@@ -27,86 +27,77 @@
     stateVersion = "21.11";
   };
 
-  programs.zsh = {
-    enable = true;
-    enableCompletion = true;
-    enableAutosuggestions = true;
-    enableSyntaxHighlighting = true;
-    defaultKeymap = "emacs";
-    plugins = [
-      {
-        name = "powerlevel10k";
-        src = pkgs.zsh-powerlevel10k;
-        file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
-      }
-      {
-        name = "powerlevel10k-config";
-        src = lib.cleanSource ./zsh;
-        file = "p10k.zsh";
-     }
-    ];
-
-    localVariables = {
-      ZSH_AUTOSUGGEST_STRATEGY = ["history" "completion"];
+  programs = {
+    zsh = {
+      enable = true;
+      enableCompletion = true;
+      enableAutosuggestions = true;
+      enableSyntaxHighlighting = true;
+      defaultKeymap = "emacs";
+      plugins = [
+        {
+          name = "powerlevel10k";
+          src = pkgs.zsh-powerlevel10k;
+          file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
+        }
+        {
+          name = "powerlevel10k-config";
+          src = lib.cleanSource ./zsh;
+          file = "p10k.zsh";
+       }
+      ];
+      localVariables = {
+        ZSH_AUTOSUGGEST_STRATEGY = ["history" "completion"];
+      };
+      initExtraBeforeCompInit = ''
+        P10KP="$XDG_CACHE_HOME/p10k-instant-prompt-''${(%):-%n}.zsh"; [[ ! -r "$P10KP" ]] || source "$P10KP"
+      '';
     };
 
-    initExtraBeforeCompInit = ''
-      P10KP="$XDG_CACHE_HOME/p10k-instant-prompt-''${(%):-%n}.zsh"; [[ ! -r "$P10KP" ]] || source "$P10KP"
-    '';
-  };
+    tmux = {
+      enable = true;
+      prefix = "C-a";
+      terminal = "screen-256color";
+      shell = "${pkgs.zsh}/bin/zsh";
+      escapeTime = 0;
+      newSession = true;
+      extraConfig = ''
+        bind-key A command-prompt 'rename-window "%%"'
+        set -g renumber-windows on
+        set -g visual-bell on
+        set -g mouse off
+        bind-key m run 'tmux show -g mouse | grep -q on && T=off || T=on;tmux set -g mouse $T;tmux display "Mouse $T"'
+        unbind -Tcopy-mode-vi Enter
+        bind-key -Tcopy-mode-vi 'v' send -X begin-selection
+        bind-key | split-window -h
+        bind-key \\ split-window -h 
+        bind-key - split-window -v
+        unbind '"'
+        unbind %
+        set -g status-interval 1
+        set -g status-bg white
+        set -g status-fg black
+        set -g status-left ' '
+        set -g status-left-length 0
+        set -g status-right ' '
+        set -g status-right-length 0
+        set-window-option -g window-status-current-style bold
+        set-window-option -g window-status-current-format '#I #W '
+        set-window-option -g window-status-format '#I #W '
+        set -g pane-active-border-style fg=white
+      '';
+    };
 
-  # TODO(negz): Configure me.
-  programs.tmux = {
-    enable = true;
-    prefix = "C-a";
-    terminal = "screen-256color";
-    shell = "${pkgs.zsh}/bin/zsh";
-    escapeTime = 0;
-    newSession = true;
+    # TODO(negz): Configure me.
+    vim = {
+      enable = true;
+    };
 
-    extraConfig = ''
-      bind-key A command-prompt 'rename-window "%%"'
-      set -g renumber-windows on
-      
-      set -g visual-bell on
-      
-      set -g mouse off
-      bind-key m run 'tmux show -g mouse | grep -q on && T=off || T=on;tmux set -g mouse $T;tmux display "Mouse $T"'
-      
-      unbind -Tcopy-mode-vi Enter
-      bind-key -Tcopy-mode-vi 'v' send -X begin-selection
-      
-      bind-key | split-window -h
-      bind-key \\ split-window -h 
-      bind-key - split-window -v
-      unbind '"'
-      unbind %
-      
-      set -g status-interval 1
-      set -g status-bg white
-      set -g status-fg black
-      set -g status-left ' '
-      set -g status-left-length 0
-      set -g status-right ' '
-      set -g status-right-length 0
-      
-      set-window-option -g window-status-current-style bold
-      set-window-option -g window-status-current-format '#I #W '
-      set-window-option -g window-status-format '#I #W '
-      
-      set -g pane-active-border-style fg=white
-    '';
-  };
-
-  # TODO(negz): Configure me.
-  programs.vim = {
-    enable = true;
-  };
-
-  programs.go = {
-    enable = true;
-    package = pkgs.go;
-    goPath = "control/go";
-    goBin = "control/go/bin";
+    go = {
+      enable = true;
+      package = pkgs.go;
+      goPath = "control/go";
+      goBin = "control/go/bin";
+    };
   };
 }
