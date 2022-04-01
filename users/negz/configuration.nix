9ab7e8327b8cdf121f4446c10e723e5d945fc1c2
Nic Cope <nicc@rk0n.org>
2022-04-01 02:01:18 -0700

    Run zsh inside nix-shell
    Signed-off-by: Nic Cope <nicc@rk0n.org>

--- users/negz/configuration.nix
+++ users/negz/configuration.nix
@@ -56,7 +56,17 @@
           name = "powerlevel10k-config";
           src = lib.cleanSource ./zsh;
           file = "p10k.zsh";
-       }
+        }
+        {
+          name = "zsh-nix-shell";
+          file = "nix-shell.plugin.zsh";
+          src = pkgs.fetchFromGitHub {
+            owner = "chisui";
+            repo = "zsh-nix-shell";
+            rev = "v0.5.0";
+            sha256 = "0za4aiwwrlawnia4f29msk822rj9bgcygw6a8a6iikiwzjjz0g91";
+        };
+      }
       ];
       localVariables = {
         ZSH_AUTOSUGGEST_STRATEGY = ["history" "completion"];
