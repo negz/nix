ac1e659130680c042e8d51d0aa9f4c4d8fad664f
Nic Cope <nicc@rk0n.org>
2022-07-02 15:44:48 -0700

    Install Parallels Tools
    Not super sure how useful this will be in my headless situation. I'm
    hopeful it might help with battery management though - e.g. Parallels
    mentions it can pause the guest OS when there are no active processes.
    
    Signed-off-by: Nic Cope <nicc@rk0n.org>

--- hosts/mael/hardware-configuration.nix
+++ hosts/mael/hardware-configuration.nix
@@ -1,6 +1,17 @@
 { config, lib, pkgs, modulesPath, ... }:
 
 {
+
+  imports = [ ../../modules/parallels-guest.nix ];
+
+  # TODO(negz): Use upstream module and package once the below PR is merged.
+  # https://github.com/NixOS/nixpkgs/pull/179582
+  disabledModules = [ "virtualisation/parallels-guest.nix" ];
+  hardware.parallels = {
+    enable = true;
+    package = (config.boot.kernelPackages.callPackage ../../pkgs/prl-tools/default.nix { });
+  };
+
   boot.initrd.availableKernelModules = [ "xhci_pci" "usbhid" "sr_mod" ];
   boot.initrd.kernelModules = [ ];
 
