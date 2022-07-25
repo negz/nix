8996e23edcce34a29c9a149b1505a86da4cfca2d
Nic Cope <nicc@rk0n.org>
2022-07-25 00:10:23 -0700

    Enable USB automounting on Roach
    ...maybe
    
    Signed-off-by: Nic Cope <nicc@rk0n.org>

--- hosts/roach/hardware-configuration.nix
+++ hosts/roach/hardware-configuration.nix
@@ -19,6 +19,12 @@
     fsType = "vfat";
   };
 
+  services = {
+    devmon = {
+      enable = true;
+    };
+  };
+
   networking.useDHCP = lib.mkDefault true;
 
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
