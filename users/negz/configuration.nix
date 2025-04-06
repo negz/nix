33461c06467635a8a8713563197db8a44c21716e
Nic Cope <nicc@rk0n.org>
2025-04-05 21:53:48 -0700

    Set nvim theme based on background color
    This'll only work at startup - it won't change the color for an open
    editor as the system changes. This is good enough for me. I don't use
    nvim as my primary IDE, so I'll rarely have files open at
    sunset/sunrise.
    
    Signed-off-by: Nic Cope <nicc@rk0n.org>

--- users/negz/configuration.nix
+++ users/negz/configuration.nix
@@ -204,7 +204,17 @@
             config = ''
               lua << END
               require('github-theme').setup {
-                vim.cmd('colorscheme github_dark_default')
+                vim.api.nvim_create_autocmd("OptionSet", {
+                  pattern = "background",
+                  callback = function()
+                    if vim.o.background == "light" then
+                      vim.cmd.colorscheme("github_light_default")
+                    end
+                    if vim.o.background == "dark" then
+                      vim.cmd.colorscheme("github_dark_default")
+                    end
+                  end,
+                })
               }
               END
             '';
