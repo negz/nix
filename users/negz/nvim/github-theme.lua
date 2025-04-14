9c28d53e5ecbc8a6718a56e62c1481159a3cb1cf
Nic Cope <nicc@rk0n.org>
2025-04-14 15:42:50 -0700

    Change bg on SIGWINCH
    Sadly doesn't work over SSH (even though tmux changes background on
    SIGWINCH...). Might work on MacOS though...
    
    Signed-off-by: Nic Cope <nicc@rk0n.org>

--- users/negz/nvim/github-theme.lua
+++ users/negz/nvim/github-theme.lua
@@ -1,14 +1,15 @@
-require('github-theme').setup {
-}
+require('github-theme').setup {}
+
+local toggle = function()
+	if vim.o.background == "light" then
+		vim.cmd.colorscheme("github_light_default")
+	end
+	if vim.o.background == "dark" then
+		vim.cmd.colorscheme("github_dark_default")
+	end
+end
 
 vim.api.nvim_create_autocmd("OptionSet", {
 	pattern = "background",
-	callback = function()
-		if vim.o.background == "light" then
-			vim.cmd.colorscheme("github_light_default")
-		end
-		if vim.o.background == "dark" then
-			vim.cmd.colorscheme("github_dark_default")
-		end
-	end,
+	callback = toggle,
 })
