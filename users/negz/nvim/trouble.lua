9f118cc217940725664931283bd99d14a8ce1b91
Nic Cope <nicc@rk0n.org>
2025-04-16 16:30:38 -0700

    Closify trouble keymaps
    Signed-off-by: Nic Cope <nicc@rk0n.org>

--- users/negz/nvim/trouble.lua
+++ users/negz/nvim/trouble.lua
@@ -2,19 +2,12 @@ local trouble = require('trouble')
 
 trouble.setup()
 
-
-local diagnostics = function()
-	trouble.toggle({ mode = "diagnostics" })
+local toggle = function(config)
+	return function()
+		trouble.toggle(config)
+	end
 end
 
-local symbols = function()
-	trouble.toggle({ mode = "symbols" })
-end
-
-local quickfix = function()
-	trouble.toggle({ mode = "quickfix" })
-end
-
-vim.keymap.set('n', '<leader>dd', diagnostics, { desc = 'Toggle Trouble diagnostics' })
-vim.keymap.set('n', '<leader>ds', symbols, { desc = 'Toggle Trouble symbols' })
-vim.keymap.set('n', '<leader>df', quickfix, { desc = 'Toggle Trouble quickfix' })
+vim.keymap.set('n', '<leader>dd', toggle({ mode = "diagnostics" }), { desc = 'Toggle Trouble diagnostics' })
+vim.keymap.set('n', '<leader>ds', toggle({ mode = "symbols" }), { desc = 'Toggle Trouble symbols' })
+vim.keymap.set('n', '<leader>df', toggle({ mode = "quickfix" }), { desc = 'Toggle Trouble quickfix' })
