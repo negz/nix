73e040e8256d9f18b6ce2a79f3ea6aedb60b8b6a
Nic Cope <nicc@rk0n.org>
2025-04-16 01:27:57 -0700

    Add a toggle for the minimap
    Signed-off-by: Nic Cope <nicc@rk0n.org>

--- users/negz/nvim/codewindow.lua
+++ users/negz/nvim/codewindow.lua
@@ -1,7 +1,12 @@
-require('codewindow').setup {
+local cw = require('codewindow')
+
+cw.setup {
 	auto_enable = true,
 	show_cursor = false,
 	window_border = 'none',
 	minimap_width = 15,
 	screen_bounds = 'background'
 }
+
+
+vim.keymap.set('n', '<leader>mt', cw.toggle_minimap, { desc = 'Toggle minimap' })
