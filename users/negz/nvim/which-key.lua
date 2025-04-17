72352f6d5da0f0d3c315dbbdadc9d04487005877
Nic Cope <nicc@rk0n.org>
2025-04-16 19:31:06 -0700

    Git util keymaps
    Signed-off-by: Nic Cope <nicc@rk0n.org>

--- users/negz/nvim/which-key.lua
+++ users/negz/nvim/which-key.lua
@@ -2,8 +2,9 @@ local wk = require('which-key')
 
 wk.add({
 	{ "<leader>t", group = "Telescope" },
-	{ "<leader>n", group = "Neotree" },
-	{ "<leader>m", group = "Codewindow" },
-	{ "<leader>d", group = "Trouble" },
-	{ "<leader>a", group = "Avante" },
+	{ "<leader>n", group = "Tree" },
+	{ "<leader>m", group = "Minimap" },
+	{ "<leader>d", group = "Diagnostics" },
+	{ "<leader>a", group = "AI" },
+	{ "<leader>g", group = "Git" },
 })
